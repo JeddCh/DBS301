99b3e0a888606791d1eb4167b043693fabcca6e5
Jedd Chionglo <59983115+JeddCh@users.noreply.github.com>
2020-02-04 12:45:05 -0500

    Delete q2.sql

--- q2.sql
+++
@@ -1,6 +0,0 @@
-select LAST_NAME, FIRST_NAME, SALARY, round(salary*1.07) "Good Salary", round(salary*0.07) "Annual Pay Increase"
-From Employees
-Where DEPARTMENT_ID = 50
-or DEPARTMENT_ID = 60
-or DEPARTMENT_ID = 20
-/
