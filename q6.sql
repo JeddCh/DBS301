181562dff97f368d0a84f9bf98d6ca2f487af773
Jedd Chionglo <59983115+JeddCh@users.noreply.github.com>
2020-02-04 12:45:28 -0500

    Delete q6.sql

--- q6.sql
+++
@@ -1,4 +0,0 @@
- SELECT last_name, to_char(hire_date,'fmDAY"," Month "the" ddsp "of year" yyyy') "Hire_Date", to_char(next_day(add_months(hire_date, 12),'Tuesday'), 'fmDAY"," Month "the" ddsp "of year" yyyy') "Review Day"
-from employees
-where hire_date > '31-Dec-97'
-/
